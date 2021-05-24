f8d770a76ce044e6e36b1685587cd730d9e4d86f
vnues <1518190293@qq.com>
2021-05-24 22:56:10 +0800

    feat: 🎸 finish config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,30 @@
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel"
+// rollup.js编译源码中的模块引用默认只支持 ES6+的模块方式import/export
+// @rollup/plugin-commonjs可以将cjs转化为es module
+// 这样做的原因 其它npm包可能是以cjs规范导出
+import commonjs from '@rollup/plugin-commonjs'
+// The @rollup/plugin-node-resolve plugin teaches Rollup how to find external modules
+import {nodeResolve} from '@rollup/plugin-node-resolve'
+import { terser } from "rollup-plugin-terser"
 
 export default {
-  input: "src/index.ts",
+  input: "src/index.tsx",
   output: {
-    file: "dist/index.cjs.js", //输出文件的路径和名称
-    format: "iife", // 五种输出格式：amd/es6/iife/umd/cjs
-    name: "bundleName", //当format为iife和umd时必须提供，将作为全局变量挂在window下
-    globals: {},
+    file: "dist/index.min.js", // 输出文件的路径和名称
+    format: "umd", // 五种输出格式：amd/es/iife/umd/cjs
+    name: "ReactFormMaker", // 当format为iife和umd时必须提供，将作为全局变量挂在window下
+    globals: {react:"React"},
+    sourcemap:true
   },
   plugins: [
     babel({
       exclude: "node_modules/**",
-    })
+      extensions:['.js', '.jsx','ts','tsx'],
+      babelHelpers:"runtime"
+    }),
+    nodeResolve(),
+    commonjs(),
+    terser()
   ],
-  external: [],
-};
+  external: ['react']
+}
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react'
+
+
+const ReactFormMaker = () => {
+  const [user, setUser] = useState({})
+  return (<div>ReactFormMaker组件</div>)
+}
+
+export default ReactFormMaker
