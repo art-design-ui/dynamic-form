05ec315ff70fbca704308882a185c655d4773d14
vnues <1518190293@qq.com>
2021-05-28 00:00:38 +0800

    feat: add docs

--- src/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/vnues/我的开源项目/dynamic-form/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
